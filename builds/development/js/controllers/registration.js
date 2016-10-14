47c08dc628220f13591850ffad889965a0dc7bc5
Boasbabs <babalolasimeon@gmail.com>
2016-10-14 14:57:47 +0100

    Add registratio form and validation

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -2,4 +2,7 @@ myAngularData.controller("RegistrationController", ["$scope", "$location", funct
   $scope.login = function() {
     $location.path("/meetings");
   }// login submission is called here
+  $scope.register = function() {
+    $location.path("/meetings");
+  }// register function is called here
 }]);// RegistrationController for registration
